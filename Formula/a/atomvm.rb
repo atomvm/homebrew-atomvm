0e2f7d3bd21ebcd37d07294fe5df4ad1bc9d4c47
Fred Dushin <fred@dushin.net>
2023-10-09 21:41:20 -0400

    Bumped version to v0.6.0-alpha.1
    Signed-off-by: Fred Dushin <fred@dushin.net>

--- Formula/a/atomvm.rb
+++ Formula/a/atomvm.rb
@@ -7,10 +7,10 @@
 class Atomvm < Formula
   desc "An Erlang VM for tiny systems"
   homepage "https://atomvm.net"
-  url "https://github.com/bettio/avm_tag_ci/archive/refs/tags/v0.6.0-alpha1-test1.tar.gz"
-  sha256 "e839e1657c7e5549b8950b56c47ec87caed3b9cf5d2e90584d0c3620c984f5b6"
+  url "https://github.com/atomvm/AtomVM/archive/refs/tags/v0.6.0-alpha.1.tar.gz"
+  sha256 "9c539b21743eacb81e6b21b60eace03a265b307ddb42203f94734ce8cb4a0084"
   license "Apache License 2.0"
-  version "v0.6.0-alpha1-test1"
+  version "v0.6.0-alpha.1"
 
   depends_on "cmake" => :build
   depends_on "gperf" => :build
@@ -20,7 +20,7 @@ class Atomvm < Formula
   depends_on "openssl@3"
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", "-D", "AVM_BUILD_RUNTIME_ONLY=on", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
